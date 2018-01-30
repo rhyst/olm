50f35c6c7273cdc5626fd48158a134e668e657c9
Rhys Tyers <rhystyers1@gmail.com>
2018-01-30 16:11:58 +0000

    Work out filenames better and factor out resuable markdown code

--- olm/article.py
+++ olm/article.py
@@ -2,49 +2,34 @@ import os
 import datetime
 import codecs
 import re
-
-INDENTATION = re.compile(r'^\s{4,}(.*)')
-META = re.compile(r'^(\w+):\s*(.*)')
-
-def md_parse_meta(text):
-    """Parse the given text into metadata and strip it for a Markdown parser.
-    :param text: text to be parsed
-    """
-    text = text.split('\n')
-    meta = {}
-    m = META.match(text[0])
-    while m:
-        key = m.group(1)
-        value = m.group(2)
-        meta[key] = value
-        text = text[1:]
-        if len(text) >= 1:
-            i = INDENTATION.match(text[0])
-            while i:
-                meta[key] = meta[key] + i.group(1)
-                text = text[1:]
-                i = INDENTATION.match(text[0])
-            m = META.match(text[0])
-        else:
-            m = False
-
-    return meta, '\n'.join(text)
+from helper import md_parse_meta
+from urllib.parse import urljoin
 
 class Article:
     """Object representing an article"""
 
-    def __init__(self, context, filepath):
-        self.source_filepath = filepath
+    def __init__(self, context, filepath=None):
         dirname = os.path.dirname(filepath)
         basename, extension = os.path.splitext(filepath)
-        self.template = context["JINJA_ENV"].get_template('article.html')
         rel_path = os.path.relpath(os.path.join(dirname, basename) + '.html', context["SOURCE_FOLDER"])
-        self.output_filepath = os.path.join(context["OUTPUT_FOLDER"], rel_path)
+        
         with codecs.open(filepath, 'r', encoding='utf8') as md_file:
             self.metadata, raw_content = md_parse_meta(md_file.read())
             self.content = context["MD"](raw_content)
-        for key in self.metadata:
-            print("{}: {}".format(key, self.metadata[key]))
+        self.date            = datetime.datetime.strptime(self.metadata['Date'].strip(), '%Y-%m-%d') if 'Date' in self.metadata else datetime.datetime.now()
+        self.source_filepath = filepath
+        self.template        = context["JINJA_ENV"].get_template('article.html')
+        self.type            = self.metadata['Type'] if 'Type' in self.metadata else ''
+        self.title           = self.metadata['Title'] if 'Title' in self.metadata else basename
+        self.summary         = context["MD"](self.metadata['Summary']) if 'Summary' in self.metadata else ''
+        self.location        = self.metadata['Location'] if 'Location' in self.metadata else None
+
+        if self.date and self.location:
+            output_filename = '{}-{}.html'.format(self.date.strftime('%Y-%m-%d'), self.location.lower())
+        else:
+            output_filename = '{}.html'.format(basename)
+        self.output_filepath = os.path.join(context["OUTPUT_FOLDER"], 'articles', output_filename)
+        self.url             = '/articles/{}'.format(output_filename)
 
     def write_file(self):
         """Write the article to a file"""
---
+++ olm/helper.py
@@ -0,0 +1,28 @@
+import re
+
+INDENTATION = re.compile(r'^\s{4,}(.*)')
+META = re.compile(r'^(\w+):\s*(.*)')
+
+def md_parse_meta(text):
+    """Parse the given text into metadata and strip it for a Markdown parser.
+    :param text: text to be parsed
+    """
+    text = text.split('\n')
+    meta = {}
+    m = META.match(text[0])
+    while m:
+        key = m.group(1)
+        value = m.group(2)
+        meta[key] = value
+        text = text[1:]
+        if len(text) >= 1:
+            i = INDENTATION.match(text[0])
+            while i:
+                meta[key] = meta[key] + i.group(1)
+                text = text[1:]
+                i = INDENTATION.match(text[0])
+            m = META.match(text[0])
+        else:
+            m = False
+
+    return meta, '\n'.join(text)
