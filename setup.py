f29206e45f3c19f97717aec1a2a409526f0b4069
Rhys Tyers <rhystyers1@gmail.com>
2018-02-19 22:55:09 +0000

    Check if summary exists

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='olm',
-    version='0.0.23',
+    version='0.0.24',
     description='Static site generator',
     url='https://github.com/rhyst/olm',
     author='Rhys Tyers',
