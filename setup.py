f9ee66e6b057989b3d056d38f958b359a4cf5e51
Rhys Tyers <rhystyers1@gmail.com>
2018-04-05 18:55:39 +0100

    upversion

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='olm',
-    version='0.0.28',
+    version='0.0.29',
     description='Static site generator',
     url='https://github.com/rhyst/olm',
     author='Rhys Tyers',
