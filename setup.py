aa3834deaaf92dd03f4afafb6200b090fdba4fc9
Rhys Tyers <rhystyers1@gmail.com>
2018-05-26 18:22:13 +0100

    Add locale option

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='olm',
-    version='0.0.35',
+    version='0.0.36',
     description='Static site generator',
     url='https://github.com/rhyst/olm',
     author='Rhys Tyers',
